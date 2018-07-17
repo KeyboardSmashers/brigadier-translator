2bf20b2942bbbb1a6f6030aa357f978ffb7fdd68
Monczak <szymon.jedrzejczak.sj@gmail.com>
2018-07-17 16:22:30 +0200

    Modify core.js

--- core.js
+++ core.js
@@ -1,5 +1,41 @@
+let commandSyntaxDictionary = {};
+
 export default class Translator {
     static translate(command) {
-        
+
+    }
+
+    static entitySelector(selector) {
+
+    }
+
+    static gamemode(gm) {
+        let result;
+
+        switch (gm) {
+            case "0":
+            case "s":
+                return "survival";
+                break;
+            case "1":
+            case "c":
+                return "creative";
+                break;
+            case "2":
+            case "a":
+                return "adventure";
+                break;
+            case "3":
+            case "sp":
+                return "survival";
+                break;
+            default: break;
+        }
+
+        return result;
+    }
+
+    static particle(name) {
+
     }
 }
