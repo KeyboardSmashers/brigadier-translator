e9c4db1896de01bd1005a196a27dcd6329b28bed
Monczak <szymon.jedrzejczak.sj@gmail.com>
2018-07-17 19:51:50 +0200

    Better UI

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,18 @@
 // Open window
-const {app, BrowserWindow, remote} = require(`electron`);
-const btns = require(`./modules/buttons`);
+const { app, BrowserWindow } = require(`electron`);
+const path = require(`path`);
 
 let win;
 
 function createWindow() {
     win = new BrowserWindow({
         width: 700, 
-        height: 320, 
+        height: 355, 
         resizable: false, 
         title: `Brigadier Translator`, 
-        frame: false
+        frame: false,
+        show: false,
+        icon: path.join(process.cwd(), `/resources/icons/bt-logo-128x128.png`)
     });
     win.loadFile(`index.html`);
 
@@ -27,8 +29,30 @@ function createWindow() {
         document.getElementById("minimize-btn").addEventListener("click", function(e) {
             w.minimize();
         });
+
+        document.getElementById("copy-btn").addEventListener("click", function(e) {
+            // Hacker Noon, you're awesome
+            const el = document.createElement("textarea");
+            el.value = document.getElementById("output-ta").value;
+            el.setAttribute('readonly', '');
+            el.style.position = 'absolute';                 
+            el.style.left = '-9999px';
+            document.body.appendChild(el);
+            const selected =            
+                document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+            el.select();
+            document.execCommand('copy');
+            document.body.removeChild(el);
+            if (selected) {
+                document.getSelection().removeAllRanges()
+                document.getSelection().addRange(selected);
+            }
+        });
     `);
     
+    win.once(`ready-to-show`, () => {
+        win.show();
+    });
 }
 
 app.on(`ready`, createWindow);
