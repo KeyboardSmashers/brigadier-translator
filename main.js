27998f26152fa2c5f6ca66700c6f49685b466b4b
Monczak <szymon.jedrzejczak.sj@gmail.com>
2018-07-17 15:49:22 +0200

    Application event handlers

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ const {app, BrowserWindow} = require(`electron`);
 
 let win;
 
-app.on(`ready`, () => {
-    win = new BrowserWindow({width: 500, height: 500});
+function createWindow() {
+    win = new BrowserWindow({width: 500, height: 300});
     win.loadFile(`index.html`);
-})
+
+    win.on(`closed`, () => {
+        win = null;
+    });
+}
+
+app.on(`ready`, createWindow);
+
+app.on(`window-all-closed`, () => {
+    if (process.platform !== `darwin`) {
+        app.quit();
+    }
+});
+
+app.on(`activate`, () => {
+    if (win === null) {
+        createWindow();
+    }
+});
