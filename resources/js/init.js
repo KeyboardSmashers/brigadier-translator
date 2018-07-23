39095a41a4ebed0aed2abaa8e6436dcb4c76a9b5
Monczak <szymon.jedrzejczak.sj@gmail.com>
2018-07-23 14:04:02 +0200

    Improved core.js

--- resources/js/init.js
+++ resources/js/init.js
@@ -1,2 +1,10 @@
 const Debug = require(`./resources/js/modules/debug.js`)
 const Translator = require(`./resources/js/modules/core.js`);
+
+document.querySelector(`#input-ta`).addEventListener(`keyup`, () => {
+    let output = Translator.translate(document.querySelector(`#input-ta`).value);
+    document.querySelector(`#output-ta`).innerHTML = output;
+    document.querySelector(`#output-label`).classList.remove(output != `` ? `visible` : `invisible`);
+    document.querySelector(`#output-label`).classList.add(output == `` ? `visible` : `invisible`);
+});
+    
--- resources/js/modules/core.js
+++ resources/js/modules/core.js
@@ -11,10 +11,42 @@ Translator.prototype.command = (input) => {
 
 }
 */
+let internalCommands = [
+    `translate`,
+    `proofread`,
+    `gamemode`
+];
+
+Translator.prototype.proofread = (input) => {
+    if (input == `undefined`) return ``;
+}
+
+Translator.prototype.gamemode = (input) => {
+    switch (input) {
+        case `0`:
+        case `s`:
+            return `survival`;
+            break;
+        case `1`:
+        case `c`:
+            return `creative`;
+            break;
+        case `2`:
+        case `a`:
+            return `adventure`;
+            break;
+        case `3`:
+        case `sp`:
+            return `spectator`;
+            break;
+        
+        
+    }
+}
 
 Translator.prototype.advancement = (input) => {
     if(input.split(` `)[1] == `test`){
-        return Debug.Error(`ADVANCEMENT TEST is now unsupported!`);
+        return `execute if entity ${input.split(` `)[2]}[advancements={${input.split(` `)[3]}=${input.split(` `)[4] != undefined ? "{" + input.split(` `)[4] + "=true}" : "true"}}] run <YOUR COMMAND HERE>`;
     } else {
         return input;
     }
@@ -33,24 +65,29 @@ Translator.prototype.banlist = (input) => {
 }
 
 Translator.prototype.blockdata = (input) => {
-    if(input.split(` `)[2] === undefined){
+    if (input.split(` `)[4] == undefined) {
         return `data get block ${input.split(` `)[1]} ${input.split(` `)[2]} ${input.split(` `)[3]}`
-    } else if(input.split(` `)[4] != undefined){
+    } else {
         return `data merge block ${input.split(` `)[1]} ${input.split(` `)[2]} ${input.split(` `)[3]} ${input.split(` `)[4]}`
     }
 }
 
 Translator.prototype.clear = (input) => {
-    return input;
+    // TEMPORARY NUMERIC IDS - WILL NEED TO BE TRANSLATED LATER
+    return `clear ${input.split(` `)[1]} ${input.split(` `)[2]}${input.split(` `)[3] != undefined ? `:${input.split(` `)[3]}` : ``}${input.split(` `)[5] != undefined ? `${input.split(` `)[5]}` : ``} ${input.split(` `)[4]}`;
 }
 
 Translator.prototype.translate = function translate(fullCommand) {
-    // JS MAGIC
-    fullCommand = fullCommand.replace(`/`, ``);
-    command = fullCommand.split(` `)[0].replace(`-`, `_`);
-    if (command == `translate`) return;
+    
+    let slashEnd = fullCommand.split(` `)[0].lastIndexOf(`/`) + 1;
+    if (/* WorldEdit Support? */ slashEnd > 1) return ``;
+    fullCommand = fullCommand[0] == `/` ? fullCommand.substring(slashEnd) : fullCommand;
+
+    command = fullCommand.split(` `)[0].replace(`-`, `_`).replace(`/`, ``);
+    if (internalCommands.includes(command)) return ``;
 
     try {
+        // JS MAGIC
         return eval(this[command])(fullCommand);
     } catch (error) {
         return ``;
